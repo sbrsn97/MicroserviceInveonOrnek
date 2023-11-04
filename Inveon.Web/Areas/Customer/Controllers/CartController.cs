c35572b7c9867753944eb353ae79266c047a7555
sbrsn <sabrisen97@gmail.com>
2023-11-04 15:04:14 +0300

    .

--- Inveon.Web/Areas/Customer/Controllers/CartController.cs
+++ Inveon.Web/Areas/Customer/Controllers/CartController.cs
@@ -10,7 +10,6 @@ namespace Inveon.Web.Areas.Customer.Controllers
     public class CartController : Controller
     {
 
-
         private readonly IProductService _productService;
         private readonly ICartService _cartService;
         private readonly ICouponService _couponService;
@@ -23,41 +22,6 @@ namespace Inveon.Web.Areas.Customer.Controllers
         public async Task<IActionResult> CartIndex()
         {
             return View(await LoadCartDtoBasedOnLoggedInUser());
-        }
-
-        private async Task<CartDto> LoadCartDtoBasedOnLoggedInUser()
-        {
-            var userId = User.Claims.Where(u => u.Type == "sub")?.FirstOrDefault()?.Value;
-            var accessToken = await HttpContext.GetTokenAsync("access_token");
-            var response = await _cartService.GetCartByUserIdAsnyc<ResponseDto>(userId, accessToken);
-
-            CartDto cartDto = new();
-            if (response != null && response.IsSuccess)
-            {
-                cartDto = JsonConvert.DeserializeObject<CartDto>(Convert.ToString(response.Result));
-            }
-
-            if (cartDto.CartHeader != null)
-            {
-
-                if (!string.IsNullOrEmpty(cartDto.CartHeader.CouponCode))
-                {
-                    var coupon = await _couponService.GetCoupon<ResponseDto>(cartDto.CartHeader.CouponCode, accessToken);
-                    if (coupon != null && coupon.IsSuccess)
-                    {
-                        var couponObj = JsonConvert.DeserializeObject<CouponDto>(Convert.ToString(coupon.Result));
-                        cartDto.CartHeader.DiscountTotal = couponObj.DiscountAmount;
-                    }
-                }
-
-                foreach (var detail in cartDto.CartDetails)
-                {
-                    cartDto.CartHeader.OrderTotal += detail.Product.Price * detail.Count;
-                }
-
-                cartDto.CartHeader.OrderTotal -= cartDto.CartHeader.DiscountTotal;
-            }
-            return cartDto;
         }
 
         [HttpPost]
@@ -95,11 +59,79 @@ namespace Inveon.Web.Areas.Customer.Controllers
             var userId = User.Claims.Where(u => u.Type == "sub")?.FirstOrDefault()?.Value;
             var accessToken = await HttpContext.GetTokenAsync("access_token");
             var response = await _cartService.RemoveFromCartAsync<ResponseDto>(cartDetailsId, accessToken);
+
+
             if (response != null && response.IsSuccess)
             {
                 return RedirectToAction(nameof(CartIndex));
             }
             return View();
+        }
+
+
+        public async Task<IActionResult> Checkout()
+        {
+            return View(await LoadCartDtoBasedOnLoggedInUser());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Checkout(CartDto cartDto)
+        {
+            try
+            {
+                var accessToken = await HttpContext.GetTokenAsync("access_token");
+                var response = await _cartService.Checkout<ResponseDto>(cartDto.CartHeader, accessToken);
+                // var response = await _cartService.Checkout2<ResponseDto>(cartDto, accessToken);
+                if (!response.IsSuccess)
+                {
+                    TempData["Error"] = response.DisplayMessage;
+                    return RedirectToAction(nameof(Checkout));
+                }
+                return RedirectToAction(nameof(Confirmation));
+            }
+            catch (Exception e)
+            {
+                return View(cartDto);
+            }
+        }
+
+        public async Task<IActionResult> Confirmation()
+        {
+            return View();
+        }
+        private async Task<CartDto> LoadCartDtoBasedOnLoggedInUser()
+        {
+            var userId = User.Claims.Where(u => u.Type == "sub")?.FirstOrDefault()?.Value;
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            var response = await _cartService.GetCartByUserIdAsnyc<ResponseDto>(userId, accessToken);
+
+            CartDto cartDto = new();
+            if (response != null && response.IsSuccess)
+            {
+                cartDto = JsonConvert.DeserializeObject<CartDto>(Convert.ToString(response.Result));
+            }
+
+            if (cartDto.CartHeader != null)
+            {
+
+                if (!string.IsNullOrEmpty(cartDto.CartHeader.CouponCode))
+                {
+                    var coupon = await _couponService.GetCoupon<ResponseDto>(cartDto.CartHeader.CouponCode, accessToken);
+                    if (coupon != null && coupon.IsSuccess)
+                    {
+                        var couponObj = JsonConvert.DeserializeObject<CouponDto>(Convert.ToString(coupon.Result));
+                        cartDto.CartHeader.DiscountTotal = couponObj.DiscountAmount;
+                    }
+                }
+
+                foreach (var detail in cartDto.CartDetails)
+                {
+                    cartDto.CartHeader.OrderTotal += detail.Product.Price * detail.Count;
+                }
+
+                cartDto.CartHeader.OrderTotal -= cartDto.CartHeader.DiscountTotal;
+            }
+            return cartDto;
         }
     }
 }
