2db2f1b007b515b4193ef8ca8fcafe5ff4ad63b3
sbrsn <sabrisen97@gmail.com>
2023-11-09 15:04:54 +0300

    minor changes on the code

--- Inveon.Services.ShoppingCartAPI/Controllers/CartAPICheckoutController.cs
+++ Inveon.Services.ShoppingCartAPI/Controllers/CartAPICheckoutController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Routing;
+using System.Globalization;
 
 namespace Inveon.Services.ShoppingCartAPI.Controllers
 {
@@ -88,58 +89,67 @@ namespace Inveon.Services.ShoppingCartAPI.Controllers
             options.SecretKey = "sandbox-Cmf9glR0awiuLlWIVhEww8WC8BjPhzJ5";
             options.BaseUrl = "https://sandbox-api.iyzipay.com";
 
-            CreatePaymentRequest request = new CreatePaymentRequest();
-            request.Locale = Locale.TR.ToString();
-            request.ConversationId = new Random().Next(1111, 9999).ToString();
+            CreatePaymentRequest request = new CreatePaymentRequest
+            {
+                Locale = Locale.TR.ToString(),
+                ConversationId = new Random().Next(1111, 9999).ToString(),
 
-            request.Currency = Currency.TRY.ToString();
-            request.Installment = 1;
-            request.BasketId = "B67832";
+                Currency = Currency.TRY.ToString(),
+                Installment = 1,
+                BasketId = "B67832"
+            };
             request.BasketId = checkoutHeaderDto.CartHeaderId.ToString();
             request.PaymentChannel = PaymentChannel.WEB.ToString();
             request.PaymentGroup = PaymentGroup.PRODUCT.ToString();
 
-            PaymentCard paymentCard = new PaymentCard();
-            paymentCard.CardHolderName = checkoutHeaderDto.CartHeaderId.ToString();
-            paymentCard.CardNumber = checkoutHeaderDto.CardNumber;
-            paymentCard.ExpireMonth = checkoutHeaderDto.ExpiryMonth;
-            paymentCard.ExpireYear = checkoutHeaderDto.ExpiryYear;
-            paymentCard.Cvc = checkoutHeaderDto.CVV;
-            paymentCard.RegisterCard = 0;
-            paymentCard.CardAlias = "Inveon";
+            PaymentCard paymentCard = new PaymentCard
+            {
+                CardHolderName = checkoutHeaderDto.CartHeaderId.ToString(),
+                CardNumber = checkoutHeaderDto.CardNumber,
+                ExpireMonth = checkoutHeaderDto.ExpiryMonth,
+                ExpireYear = checkoutHeaderDto.ExpiryYear,
+                Cvc = checkoutHeaderDto.CVV,
+                RegisterCard = 0,
+                CardAlias = "Inveon"
+            };
             request.PaymentCard = paymentCard;
 
-            Buyer buyer = new Buyer();
-            //buyer.Id = cartDto.CartHeader.UserId;
-            buyer.Id = "BY789";
-            buyer.Name = checkoutHeaderDto.FirstName;
-            buyer.Surname = checkoutHeaderDto.FirstName;
-            buyer.GsmNumber = checkoutHeaderDto.Phone;
-            buyer.Email = checkoutHeaderDto.Email;
-            buyer.IdentityNumber = "74300864791";
-            buyer.LastLoginDate = "2015-10-05 12:43:35";
-            buyer.RegistrationDate = "2013-04-21 15:12:09";
-            buyer.RegistrationAddress = "Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1";
-            buyer.Ip = "85.34.78.112";
-            buyer.City = "Istanbul";
-            buyer.Country = "Turkey";
-            buyer.ZipCode = "34732";
+            Buyer buyer = new Buyer
+            {
+                Id = checkoutHeaderDto.UserId,
+                Name = checkoutHeaderDto.FirstName,
+                Surname = checkoutHeaderDto.LastName,
+                GsmNumber = checkoutHeaderDto.Phone,
+                Email = checkoutHeaderDto.Email,
+                IdentityNumber = "74300864791",
+                LastLoginDate = "2015-10-05 12:43:35",
+                RegistrationDate = "2013-04-21 15:12:09",
+                RegistrationAddress = "Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1",
+                Ip = "85.34.78.112",
+                City = "Istanbul",
+                Country = "Turkey",
+                ZipCode = "34732"
+            };
             request.Buyer = buyer;
 
-            Address shippingAddress = new Address();
-            shippingAddress.ContactName = "Jane Doe";
-            shippingAddress.City = "Istanbul";
-            shippingAddress.Country = "Turkey";
-            shippingAddress.Description = "Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1";
-            shippingAddress.ZipCode = "34742";
+            Address shippingAddress = new Address
+            {
+                ContactName = $"{checkoutHeaderDto.FirstName} {checkoutHeaderDto.LastName}",
+                City = "Istanbul",
+                Country = "Turkey",
+                Description = "Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1",
+                ZipCode = "34742"
+            };
             request.ShippingAddress = shippingAddress;
 
-            Address billingAddress = new Address();
-            billingAddress.ContactName = "Jane Doe";
-            billingAddress.City = "Istanbul";
-            billingAddress.Country = "Turkey";
-            billingAddress.Description = "Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1";
-            billingAddress.ZipCode = "34742";
+            Address billingAddress = new Address
+            {
+                ContactName = $"{checkoutHeaderDto.FirstName} {checkoutHeaderDto.LastName}",
+                City = "Istanbul",
+                Country = "Turkey",
+                Description = "Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1",
+                ZipCode = "34742"
+            };
             request.BillingAddress = billingAddress;
 
             List<BasketItem> basketItems = new List<BasketItem>();
@@ -151,8 +161,8 @@ namespace Inveon.Services.ShoppingCartAPI.Controllers
                 basketItem.Id = "BI10" + product.ProductId.ToString();
                 basketItem.Name = product.Name;
                 basketItem.Category1 = product.CategoryName;
-                basketItem.Price = (product.Price * cartDetail.Count).ToString();
-                basketItem.ItemType = BasketItemType.PHYSICAL.ToString();               
+                basketItem.Price = (product.Price * cartDetail.Count).ToString(CultureInfo.InvariantCulture);
+                basketItem.ItemType = BasketItemType.PHYSICAL.ToString();
 
                 basketItems.Add(basketItem);
             }
