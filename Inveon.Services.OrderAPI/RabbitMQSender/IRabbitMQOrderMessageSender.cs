bed3e5bdffdc8d1dfeef117a731e6c6720c79f18
sbrsn <sabrisen97@gmail.com>
2023-11-12 14:33:32 +0300

    orderapi has been created

---
+++ Inveon.Services.OrderAPI/RabbitMQSender/IRabbitMQOrderMessageSender.cs
@@ -0,0 +1,9 @@
+﻿using Inveon.MessageBus;
+
+namespace Inveon.Services.OrderAPI.RabbitMQSender
+{
+    public interface IRabbitMQOrderMessageSender
+    {
+        void SendMessage(BaseMessage baseMessage, String queueName);
+    }
+}
